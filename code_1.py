311694a2250a2f8106319a5103b041ff1385c2a3
5ToYou <sobkos934@gmail.com>
2025-10-15 16:47:47 +0300

    jhh

--- code_1.py
+++ code_1.py
@@ -23,13 +23,50 @@ class graph:
             for val in row:
                 print(val,end=" "),
             print()
+
+
+    def dfs(self, start, visited=None):
+        if visited is None:
+            visited = [False] * self.size
+
+        visited[start] = True
+        print(start, end=" ")
+
+        for i in range(self.size):
+            if self.matrix[start][i] == 1 and not visited[i]:
+                self.dfs(i, visited)
+
+    # BFS (в ширину) без deque
+    def bfs(self, start):
+        visited = [False] * self.size
+        queue = [start]
+        visited[start] = True
+
+        while queue:
+            vertex = queue.pop(0)  # забираємо перший елемент
+            print(vertex, end=" ")
+
+            for i in range(self.size):
+                if self.matrix[vertex][i] == 1 and not visited[i]:
+                    queue.append(i)
+                    visited[i] = True
         
         
 if __name__ == "__main__":
-    g = graph(5)
-    g.add_edge(0,3)
+    g = graph(6)
     g.add_edge(0,1)
     g.add_edge(0,2)
-    g.add_edge(1,2)
+    g.add_edge(1,3)
+    g.add_edge(2,1)
+    g.add_edge(3,4)
+    g.add_edge(4,5)
+    g.add_edge(1,5)
+    g.add_edge(2,4)
     
     g.display()
+
+    print("\nDFS starting from vertex 0:")
+    g.dfs(0)
+
+    print("\nBFS starting from vertex 0:")
+    g.bfs(0)
