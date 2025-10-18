347fb604be92369078db237ece1fc56fa1dac317
5ToYou <sobkos934@gmail.com>
2025-10-18 14:20:47 +0300

    99.9% of gamblers quit before hitting big

--- IdleDeathGamble.py
+++ IdleDeathGamble.py
@@ -1,24 +1,44 @@
 import random
 
+tickets = 1000
+
+print(" if you need more tickets press 1")
+
 while True:
     slot1 = random.randint(1,9)
     slot2 = random.randint(1,9)
     slot3 = random.randint(1,9)
 
+    
+    print(f"You have {tickets} tickets")
 
-
-    print(f" = {slot1} {slot2} {slot3} = ")
+    
 
     if slot1 == slot2 == slot3:
+        tickets += 10000
+        print(f" = {slot1} {slot2} {slot3} = ")
         print("  JACKPOT")
-        break
+
+
 
 
     choice = input("Press Enter to roll: ")
     match choice:
         case "":
-            pass
-        
+            if tickets <= 0:
+                print()
+                print("Out of tickets")
+                print()
+            if tickets >= 100:
+                tickets -= 100
+                print()
+                print(f" = {slot1} {slot2} {slot3} = ")
+                print()
+
+        case "1":
+            tickets += 1000
+            print("You got 1000 tickets")
+
         case _:
             print("99.9% of gamblers quit before hitting big")
             break
