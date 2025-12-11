2fdaa221a0ef3af8a04bc89a7b33fa2299bf5c1c
5ToYou <sobkos934@gmail.com>
2025-12-11 18:22:40 +0200

    lal

--- NoOneNeedsThis/IMGONNADOIT.py
+++ NoOneNeedsThis/IMGONNADOIT.py
@@ -2,29 +2,18 @@ solution_set = []
 coins = [5,2,1]
 
 start_sum = 0
-need_sum = 16
+need_sum = 18
+remaining_sum = need_sum
 
-while start_sum != need_sum:
-    i = len(solution_set)
-    start_sum += coins[0]
-    solution_set.append(coins[0])
-    
-    if start_sum >= need_sum:
-        x = 0
-        while x < 2:
-            x += 1
-            solution_set.pop(i - 1)
 
-            start_sum -= coins[0]
-            start_sum += coins[1]
-            solution_set.append(coins[1])
+for coin in coins:
+    while remaining_sum >= coin:
+        solution_set.append(coin)
+        remaining_sum -= coin
 
-    if start_sum >= need_sum:
-        solution_set.pop(i - 1)
-        start_sum -= coins[1]
-        start_sum += coins[2]
-        solution_set.append(coins[2])
-
-print(solution_set)
+if remaining_sum == 0:
+    print(solution_set)
+else:
+    print("_")
 
 
